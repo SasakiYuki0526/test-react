1497325906e079f0714425e005ba71f9aa59b033
yuki <10756044@ntub.edu.tw>
2022-03-01 03:24:46 +0800

    add class-based way

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Counter from './components/Counter'; // 引用時要import進來
 
+// hooks way 寫法
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <Counter />
+      <Counter count={count} setCount={setCount} title="Counter App"/>
     </div>
   )
 }
 
 export default App;
+
+// class-based 寫法
+// class App extends Component {
+//   state ={
+//     count:0
+//   }
+//   handleCountChange =() => {
+//     this.setState({count:this.state.count+1})
+//   }
+
+//   render() {
+//     return(
+//       <div>
+//         <div className="App">
+//           <Counter count={this.state.count} setCount={this.state.handleCountChange}/>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,23 @@
 import React ,{useState} from "react";
+import PropTypes from "prop-types";
 
 // const(常數):常數不能重複指定值，也不能重複宣告。
-const Counter = () => {
+const Counter = (props) => {
     const [count, setCount] = useState(0);
 
     return (
         <div>
-            <p>count:{count}</p>
-            <button onClick={() =>setCount(count+1)}>+</button>
+            <h3>{props.title}</h3>
+            <p>count:{props.count}</p>
+            <button onClick={() =>props.setCount(props.count+1)}>+</button>
         </div>
     )
+}
+
+Counter.prototypes={ // 型別檢查
+    count: PropTypes.number,
+    setCount: PropTypes.function,
+    title: PropTypes.string
 }
 
 export default Counter;
